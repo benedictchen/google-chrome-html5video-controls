4dd3c667bc65897deffa333d1ce64af7d898f3b0
Harsh Sharma <harsh.sharma@sevenlakes.com>
2017-10-10 00:22:57 +0546

    Rename keys to keysCombo

--- options.js
+++ options.js
@@ -1,10 +1,10 @@
 // Saves options to chrome.storage
 function save_options() {
   var increment = document.getElementById('increment').value;
-  var keys = document.getElementById('keys').checked;
+  var keyCombo = document.getElementById('keyCombo').value;
   chrome.storage.sync.set({
     increment: increment,
-    keys: keys
+    keyCombo: keyCombo
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -20,10 +20,10 @@ function save_options() {
 function restore_options() {
   chrome.storage.sync.get({
     increment: '0.1',
-    keys: "udar"
+    keyCombo: 'udar'
   }, function(items) {
-    document.getElementById('increment').value = items.favoriteColor;
-    document.getElementById('keys').checked = items.likesColor;
+    document.getElementById('increment').value = items.increment;
+    document.getElementById('keyCombo').value = items.keyCombo;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
